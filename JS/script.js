9829dd02c349280ccf3be5e718c64d4c40c5e080
KKaiKK <KKaiKK@web.de>
2015-03-13 20:32:02 +0100

    Implementierung der Grundstruktur
    index, CSS und JS Files hinzugefügt.

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+function change(){
+
+    document.getElementById("change").innerHTML = "Ich habe mich verändert.";
+    document.getElementById("changeAgain").innerHTML = "Verändere weiter!";
+
+}
+
+function changeAgain(){
+
+    document.getElementById("change").innerHTML = "Ich habe mich wieder verändert.";
+
+
+}
