0c50d2d774c272c43f64fe56f12d8d2a4fbe506e
KKaiKK <KKaiKK@web.de>
2015-03-16 19:51:24 +0100

    onmouseover und onmouseout hinzugefügt
    onmouseover und onmouseout hinzugefügt

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 function change(){
 
     document.getElementById("change").innerHTML = "Ich habe mich verändert.";
+    document.getElementById("aChange").style.backgroundColor="lightblue";
     document.getElementById("changeAgain").innerHTML = "Verändere weiter!";
 
 }
@@ -9,5 +10,17 @@ function changeAgain(){
 
     document.getElementById("change").innerHTML = "Ich habe mich wieder verändert.";
 
+
+}
+
+function hover(){
+
+    document.getElementById("hover").style.color ="green";
+
+}
+
+function nohover(){
+
+    document.getElementById("hover").style.color ="black";
 
 }
